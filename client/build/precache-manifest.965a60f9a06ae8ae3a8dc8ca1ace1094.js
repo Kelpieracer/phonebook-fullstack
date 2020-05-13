f39b70ca8c659c5efc97d8cb41fb0f46690bd534
Tapio Mäntysalo <tapio.m.mantysalo@utu.fi>
2020-05-13 22:22:22 +0300

    removed client/build from .gitignore

---
+++ client/build/precache-manifest.965a60f9a06ae8ae3a8dc8ca1ace1094.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703db5cea3f0d7e99d3bde5a14a04c13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2d5cbc086577ef9779a",
+    "url": "/static/css/main.f0db8f21.chunk.css"
+  },
+  {
+    "revision": "9edd48581bce454f579b",
+    "url": "/static/js/2.9fb8b823.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.9fb8b823.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d5cbc086577ef9779a",
+    "url": "/static/js/main.246f29fe.chunk.js"
+  },
+  {
+    "revision": "2e302b0192b4c4207747",
+    "url": "/static/js/runtime-main.629ef19f.js"
+  }
+]);
